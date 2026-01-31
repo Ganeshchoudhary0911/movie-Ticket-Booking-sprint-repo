31ddcc27ebb936ff46d82079fb01db7f1643daa1
Choudhary <ganesh.a.choudhary@capgemini.com>
2026-01-31 20:31:36 +0530

    movie entity

--- 1-MovieTicketBooking/src/main/java/com/cg/repository/MovieReposistory.java
+++ 1-MovieTicketBooking/src/main/java/com/cg/repository/MovieReposistory.java
@@ -1,7 +1,15 @@
 package com.cg.repository;
 
+import java.util.List;
 
-public class MovieReposistory  {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.cg.entity.Movie;
+
+@Repository
+public interface MovieReposistory extends JpaRepository<Movie, Long> {
 	
-	    
+	    // Custom query to search movies by name
+	    List<Movie> findByMovieNameContainingIgnoreCase(String movieName);
 	}
