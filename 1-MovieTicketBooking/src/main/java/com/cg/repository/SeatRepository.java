529c35ff8b59be3ed6b49d6f7172c5b4a52a4ceb
karelaks <karedla.lakshmi-sowjanya@capgemini.com>
2026-02-02 16:04:15 +0530

    project

--- 1-MovieTicketBooking/src/main/java/com/cg/repository/SeatRepository.java
+++ 1-MovieTicketBooking/src/main/java/com/cg/repository/SeatRepository.java
@@ -1,19 +1,11 @@
 package com.cg.repository;
-	 
+ 
 import com.cg.entity.Seat;
-
-	import com.cg.entity.Show;
-	import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
+import com.cg.entity.Show;
+import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
-
-@Repository
-	public interface SeatRepository extends JpaRepository<Seat, Long> {
-	 
-	//get all seats for a show
-	    List<Seat> findByShowShowId(Long showId);
-	
-	//get only available for a show    
-	    List<Seat> findByShowShowIdAndIsBookedFalse(Long showId);
+ 
+public interface SeatRepository extends JpaRepository<Seat, Long> {
+ 
+    List<Seat> findByShow(Show show);
 }
--- 1-MovieTicketBooking/src/main/java/com/cg/service/SeatService.java
+++ 1-MovieTicketBooking/src/main/java/com/cg/service/SeatService.java
@@ -1,45 +1,34 @@
 package com.cg.service;
-
-
-import java.util.List;
-
+ 
+import com.cg.entity.Seat;
+import com.cg.entity.Show;
+import com.cg.repository.SeatRepository;
+import com.cg.service.ISeatService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import com.cg.entity.Seat;
-import com.cg.repository.SeatRepository;
-
+ 
+import java.util.List;
+ 
 @Service
-public class SeatService {
-
-	@Autowired
-	private SeatRepository seatRepository;
-	
-	//Add seat
-	public Seat addSeat(Seat seat) {
-		seat.setBooked(false);         //default value
-		return seatRepository.save(seat);
-	}
-	
-	//Get all seats for show
-	public List<Seat> getSeatByShow(Long showId){
-		return seatRepository.findByShowShowId(showId);
-	}
-	
-	// Get available seats for a show
-    public List<Seat> getAvailableSeats(Long showId) {
-        return seatRepository.findByShowShowIdAndIsBookedFalse(showId);
+public class SeatService implements ISeatService {
+ 
+    @Autowired
+    private SeatRepository seatRepository;
+ 
+    @Override
+    public List<Seat> getSeatsByShow(Show show) {
+        return seatRepository.findByShow(show);
     }
  
-    // Book a seat
-    public Seat bookSeat(Long seatId) {
-        Seat seat = seatRepository.findById(seatId)
+    @Override
+    public Seat getSeatById(Long seatId) {
+        return seatRepository.findById(seatId)
                 .orElseThrow(() -> new RuntimeException("Seat not found"));
+    }
  
-        if (seat.isBooked()) {
-            throw new RuntimeException("Seat already booked");
-        }
- 
+    @Override
+    public Seat markSeatAsBooked(Long seatId) {
+        Seat seat = getSeatById(seatId);
         seat.setBooked(true);
         return seatRepository.save(seat);
     }
